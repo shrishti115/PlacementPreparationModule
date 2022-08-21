3516bc4f6035dbf70eb71dd0c24c69f414cc69ad
shrishti115 <66818254+shrishti115@users.noreply.github.com>
2022-08-21 10:24:00 +0530

    Time: 1 ms (89.85%), Space: 44 MB (15.26%) - LeetHub

--- 50-powx-n/50-powx-n.java
+++ 50-powx-n/50-powx-n.java
@@ -2,7 +2,8 @@ class Solution {
     public double myPow(double x, int n) {
         if (n == 0) return 1;
         if (n == 1) return x;
-        else if (n == -1) return 1 / x;
+        if (n == -1) 
+            return 1 / x;
         double res = myPow(x, n / 2);
         if (n % 2 == 0) return res * res;
         else if (n % 2 == -1) return res * res * (1/x);
